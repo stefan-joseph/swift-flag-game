6ffab5f0eadda396eba5fca4230f3810d1ce770a
Stefan Page <90534062+stefpage@users.noreply.github.com>
2023-04-20 12:59:56 -0600

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Stefan Page on 2023-04-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
